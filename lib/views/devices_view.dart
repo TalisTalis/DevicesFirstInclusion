4b126c8ab3c08276cc9cdc6888c87f6ca0197654
TalisTalis <tsarst@gmail.com>
2022-12-02 22:20:17 +0300

    deletee ios web linux win

---
+++ lib/views/devices_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DevicesView extends StatelessWidget {
+  const DevicesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
